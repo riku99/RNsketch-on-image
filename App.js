66e4a6fef1565d5f2ccc8852eae8b3f60f849a7a
riku <riku.suny@gmail.com>
2021-03-29 19:17:22 +0900

    Fix stroke width

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import {View, Image, Dimensions, StyleSheet} from 'react-native';
-import {ImageEditor} from '@wwimmo/react-native-sketch-canvas';
+import React, {useRef, useState} from 'react';
+import {View, Image, Dimensions, StyleSheet, Text, Button} from 'react-native';
+import RNImageEditor from '@wwimmo/react-native-sketch-canvas';
 
 const flight = require('./flight.jpg');
 
 const App = () => {
+  const [strokeW, setStrokeW] = useState(20);
+  const ref = useRef();
   return (
     <View style={styles.container}>
       <Image
@@ -12,11 +14,52 @@ const App = () => {
         resizeMode="contain"
         style={{width, height: '100%'}}
       />
-      <ImageEditor
-        style={styles.canvas}
-        strokeColor={'black'}
-        strokeWidth={7}
+      <RNImageEditor
+        ref={ref}
+        containerStyle={styles.canvas}
+        canvasStyle={{backgroundColor: 'transparent', flex: 1}}
+        defaultStrokeIndex={0}
+        strokeComponent={color => (
+          <View style={[{backgroundColor: color}, styles.strokeColorButton]} />
+        )}
+        strokeSelectedComponent={color => {
+          return (
+            <View
+              style={[
+                {backgroundColor: color, borderWidth: 2},
+                styles.strokeColorButton,
+              ]}
+            />
+          );
+        }}
+        strokeWidthComponent={w => {
+          return (
+            <View style={styles.strokeWidthButton}>
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  marginHorizontal: 2.5,
+                  width: Math.sqrt(w / 3) * 5,
+                  height: Math.sqrt(w / 3) * 5,
+                  borderRadius: (Math.sqrt(w / 3) * 10) / 2,
+                }}
+              />
+            </View>
+          );
+        }}
+        strokeWidthStep={0.5}
       />
+      <View style={{position: 'absolute', bottom: 100}}>
+        <Button
+          title="Undo"
+          onPress={() => {
+            if (ref.current) {
+              ref.current.undo();
+              setStrokeW(5);
+            }
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -26,6 +69,9 @@ const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   canvas: {
     position: 'absolute',
@@ -33,6 +79,34 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
+    backgroundColor: 'transparent',
+  },
+  functionButton: {
+    position: 'absolute',
+    top: 100,
+    left: 100,
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: 'red',
+    zIndex: 100,
+  },
+  strokeColorButton: {
+    marginHorizontal: 5,
+    marginVertical: 30,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  strokeWidthButton: {
+    marginHorizontal: 2.5,
+    marginVertical: 20,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#39579A',
   },
 });
 
